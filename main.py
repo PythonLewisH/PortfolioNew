533c74665d55764c6baf4818b6bd6dcab4f64332
lewishudson <pythonlewish@gmail.com>
2022-11-22 19:45:00 +1300

    email edit

--- main.py
+++ main.py
@@ -16,23 +16,28 @@ def home():
         return render_template("index.html", contact_form=contact_form)
     else:
         if contact_form.validate_on_submit():
-            enquiry_name = contact_form.name.data
-            enquiry_email = contact_form.email.data
-            enquiry_message = contact_form.message.data
+            try:
+                enquiry_name = contact_form.name.data
+                enquiry_email = contact_form.email.data
+                enquiry_message = contact_form.message.data
 
-            password = os.getenv("PASSWORD")
-            my_email = "lewishudsonpro@outlook.com"
-            subject = "ENQUIRY RECEIVED FROM WEBSITE"
-            message = 'Subject: {}\n\n{}'.format(subject, f"Enquirer email: {enquiry_email} \n" + f"Enquirer name: "
-                                                                                                  f"{enquiry_name} \n" +
-                                                 f"Enquirer message: {enquiry_message}")
-            server = smtplib.SMTP("smtp-mail.outlook.com", 587)
-            server.starttls()
-            server.login(my_email, password)
-            server.sendmail(my_email, my_email, message)
-            server.close()
-            flash("Thank you for your enquiry. We will get back to you within 48 hours.")
-            return render_template("index.html", contact_form=contact_form)
+                password = os.getenv("PASSWORD")
+                my_email = "lewishudsonpro@outlook.com"
+                subject = "ENQUIRY RECEIVED FROM WEBSITE"
+                message = 'Subject: {}\n\n{}'.format(subject, f"Enquirer email: {enquiry_email} \n" + f"Enquirer name: "
+                                                                                                      f"{enquiry_name} \n" +
+                                                     f"Enquirer message: {enquiry_message}")
+                server = smtplib.SMTP("smtp-mail.outlook.com", 587)
+                server.starttls()
+                server.login(my_email, password)
+                server.sendmail(my_email, my_email, message)
+                server.close()
+                flash("Thank you for your enquiry. We will get back to you within 48 hours.")
+                return render_template("index.html", contact_form=contact_form)
+            except:
+                flash("Something went wrong and we could not deliver your message, You can try sending an email instead.")
+                print(contact_form.errors)
+                return render_template("index.html", contact_form=contact_form)
         else:
             flash("Something went wrong and we could not deliver your message, try sending an email instead.")
             print(contact_form.errors)
